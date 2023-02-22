4b213026aa8067b683c41110400a9882ae9f3644
eichitakaya <takaya.wisdom0616@gmail.com>
2023-02-22 17:14:51 +0900

    added active_selection.py

---
+++ scripts/active_selection.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def select_init(volume) -> np.ndarray:
+    
+    return X, Selected_T, GT
+
+def generate_feature_series():
+    return feature_series
--- scripts/data_loader.py
+++ scripts/data_loader.py
@@ -14,35 +14,54 @@ class DatasetForSemi(torch.utils.data.Dataset):
     
 class DataLoaderFor4s:
     def __init__(self, target_volume):
-        self.target_volume_path: str = get_dataset_name(target_volume) # The kind of organ or lesion. (str)
-        self.patient_id_list: list = get_patient_id_list()
+        self.target_volume_path: str = get_dataset_name(target_volume)[0] # The kind of organ or lesion. (str)
+        self.target_z_axis: int = get_dataset_name(target_volume)[1]
+        self.patient_id_list: list = self.get_patient_id_list()
         #self.volume_id_list: list = get_volumes() # The length of this list is corresponding to the total number of organ or lesion.
         
-        self.volumes: list = [] # テンソル型に変換するのは呼び出された後でよい？
+        #self.volumes: list = self.id_to_volumes(self.patient_id_list[0]) # テンソル型に変換するのは呼び出された後でよい？
     
     def get_patient_id_list(self):
-        path = self.target_volume
+        path = self.target_volume_path
         id_list = glob.glob(path + "*.nii*")
         return id_list
     
-    def get_volumes(self, patient_id):
-        # 全てのラベルが連続している（真っ黒な画像を挟まない）症例のみを抽出
-        # とりあえず，heart, spleen, hippocampusは連続していることにしておく
-        # １症例に複数の病変が含まれる場合については，後回し
-        
-        volumes: list = glob.glob()
-        return volumes
     
-    def id_to_volumes(self, patient_id):
+    def id_to_volumes(self, patient_id) -> list:
         # 全てのラベルが連続している（真っ黒な画像を挟まない）症例のみを抽出
         # とりあえず，heart, spleen, hippocampusは連続していることにしておく
         # １症例に複数の病変が含まれる場合については，後回し
         # __getitem__の中で使う
-        return
+        # 要素はnp.array型(スライス数，h, w)
+        # 要素数はvolume数となる
+        volume_list = []
+        
+        arr_X = nib.load(patient_id).get_data()
+        arr_T = nib.load(patient_id.replace("imagesTr", "labelsTr")).get_data()
+        # 第1軸がスライス枚数となるように変換
+        arr_X = axis_transpose(arr_X, self.target_z_axis)
+        arr_T = axis_transpose(arr_T, self.target_z_axis)
+        labeled_volume_X, labeled_volume_T = self._extract_label_volumes(arr_X, arr_T)
+        return labeled_volume_X, labeled_volume_T
+    
+    def _extract_label_volumes(self, volume_X, volume_T) -> list:
+        index_memo = []
+        for i in range(volume_T.shape[0]):
+            if volume_T[i].sum() != 0:
+                index_memo.append(i)
+        labeled_volume_X = np.zeros((len(index_memo), volume_T.shape[1], volume_T.shape[2]))
+        labeled_volume_T = np.zeros_like(labeled_volume_X)
+        
+        for i in range(len(index_memo)):
+            labeled_volume_X[i] = volume_X[index_memo[i]]
+            labeled_volume_T[i] = volume_T[index_memo[i]]
+        
+        return labeled_volume_X, labeled_volume_T
     
     def __getitem__(self, item):
         patient_id = self.patient_id_list[item]
-        return id_to_volumes(patient_id)
+        X, T = self.id_to_volumes(patient_id)
+        return X, T
 
 class GroupWiseSplit():
     def __init__(self, target_volume, val_ratio, group_shuffle):
@@ -68,6 +87,8 @@ class GroupWiseSplit():
         train_groups: list = groups[:train_n]
         val_groups: list = groups[train_n:train_n+val_n]
         return train_groups, val_groups
+    
+    
     
 class DataSetFor2DSegmentation(torch.utils.data.Dataset):
     def __init__(self, groups, target_focus=False, transform=None, train=True):
@@ -142,11 +163,21 @@ class Decathlon(torch.utils.data.Dataset):
 
 
 def get_dataset_name(name):
-    name_path_dict = {"hippocampus":"/takaya_workspace/Medical_AI/data/decathlon/Task04_Hippocampus/imagesTr/",
-                      "heart":"/takaya_workspace/Medical_AI/data/decathlon/Task02_Heart/imagesTr/",
-                      "spleen":"/takaya_workspace/Medical_AI/data/decathlon/Task09_Spleen/imagesTr/"}
+    # hippocampusは後でちゃんと確認
+    name_path_dict = {"hippocampus":["/takaya_workspace/Medical_AI/data/decathlon/Task04_Hippocampus/imagesTr/", 1],
+                      "heart":["/takaya_workspace/Medical_AI/data/decathlon/Task02_Heart/imagesTr/", 2],
+                      "spleen":["/takaya_workspace/Medical_AI/data/decathlon/Task09_Spleen/imagesTr/", 2]}
     return name_path_dict[name]
 
+def axis_transpose(arr, z_axis):
+    if z_axis == 0:
+        return arr
+    elif z_axis == 1:
+        return np.transpose(arr, (1, 0, 2))
+    elif z_axis == 2:
+        return np.transpose(arr, (2, 0, 1))
+        
+    
 if __name__ == "__main__":
     data = DataLoaderFor4s("heart")
-    print(data.target_volume)
+    print(data[1][0].shape, data[1][1].shape)
